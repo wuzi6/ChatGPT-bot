53f919bc8b83b3669e2fb5dd25c43a004bed22f1
wdsagg6 <m.v.derugin6@gmail.com>
2023-02-07 15:44:40 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+#Імпортуємо бібліотеки
+
+import openai
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
+#Токени
+token = ''  #Сюди вставляємо токен, який надав вам @BotFather
+openai.api_key = ''  #API Key беремо з сайту https://platform.openai.com
+
+#Скрипт
+
+bot = Bot(token)
+dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def send(message : types.Message):
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=message.text,
+    temperature=0.9,
+    max_tokens=1000,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.6,
+    stop=["You:"]
+)
+    await message.answer(response['choices'][0]['text'])
+
+executor.start_polling(dp, skip_updates=True)
